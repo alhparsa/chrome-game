7d4bf457579428e84e2eaf50c6e45e6a8cc9da2a
hyoon1998 <hansooyoon@hotmail.com>
2018-05-06 22:06:59 +0800

    non-moving objects

--- game.py
+++ game.py
@@ -1,32 +1,27 @@
-import pygame
+import pygame,random
 pygame.init()
 win=pygame.display.set_mode((500,500))
 pygame.display.set_caption("First Game")
-colours = {"Red": (255,0,0), "Green": (0,255,0),
-          "White": (255,255,255),"Blue": (0,0,255),
-          "Black": (0,0,0)}
-
-
+colours = {"Red":   (255,0,0),      "Green":    (0,255,0),
+          "White":  (255,255,255),  "Blue":     (0,0,255),
+          "Black":  (0,0,0)}
 run =True
-
-x=120
-y=250
+x=80
+y=350
 width=10
 height=30
 vel=6
 init_vel = vel
 init=y
-accel = -1
+accel = -1 
 pressed = False
-counter = 0
-mult=0
-reset=False
-count_1=0
-
+counter = 2
+obs_maxh=65
+obs_minh=35
+obs_endh=random.randint(obs_minh,obs_maxh)
 while run:
-    pygame.time.delay(15)
+    pygame.time.delay(13)
     if  pressed == True:
-        count_1+=1
         vel += accel
         y -= vel
     for event in pygame.event.get():
@@ -41,13 +36,16 @@ while run:
             vel = init_vel
             y=init
             pressed = False
-        elif(counter>0 and pressed==False):
+        elif(counter>2 and pressed==False):
             vel*=round(counter/(1.5**2.8))
             pressed = True
-            counter = 0
+            counter = 2
 
+    
+    
     win.fill(colours["White"])
-    pygame.draw.rect(win,colours["Red"],(x,y,width,height))
+    pygame.draw.rect(win,colours["Red"],(x,y-height,width,height))
+    pygame.draw.rect(win,colours["Blue"],(250,350-obs_endh,width,obs_endh))
     pygame.display.update()
             
 pygame.quit()
