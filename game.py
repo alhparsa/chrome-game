5cbfc582fe9e295033a8b37a370fc2ce403fbc69
Parsa <alh.parsa@gmail.com>
2018-05-10 23:13:09 +0800

    barriers fixed, collosion detections added

--- game.py
+++ game.py
@@ -1,16 +1,15 @@
 import pygame
 import random
+from datetime import datetime
+from math import atan
 
-pygame.init()
-win = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("TRex")
 colours = {"Red": (255, 0, 0), "Green": (0, 255, 0),
            "White": (255, 255, 255), "Blue": (0, 0, 255),
            "Black": (0, 0, 0)}
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, screen, width=10, height=30, x=80, y=350, velocity=6, acceleration=-1, color=colours["Blue"]):
+    def __init__(self, width=10, height=30, x=80, y=350, velocity=6, acceleration=-1, color=colours["Blue"]):
         super(Player, self).__init__()
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
@@ -19,7 +18,6 @@ class Player(pygame.sprite.Sprite):
         self.counter = 2
         self.init_velocity = velocity
         self.init = y
-        self.screen = screen
         self.acceleration = acceleration
         self.velocity = velocity
         self.rect.x = x
@@ -52,7 +50,7 @@ class Player(pygame.sprite.Sprite):
 
 class Barrier(pygame.sprite.Sprite):
 
-    def __init__(self, width=10, height=random.randint(0, 65), x_velocity=-3, color=colours["Green"], screen_width=500,
+    def __init__(self, width=10, height=0, x_velocity=-3, color=colours["Green"], screen_width=500,
                  player_y_position=350):
         super(Barrier, self).__init__()
         self.x_velocity = x_velocity
@@ -65,49 +63,69 @@ class Barrier(pygame.sprite.Sprite):
 
     def height_randomizer(self):
         self.height = random.randint(15, 45)
+
     def update(self):
         self.rect.x += self.x_velocity
-            
 
 
-#barrier = Barrier()
+def outside_frame(barriers):
+    for s in barriers.sprites():
+        if s.rect.x < 0 - s.rect.width:
+            barriers.remove(s)
+    barriers.update()
+    return barriers
+
+
+def barrier_generator(barriers, speed_change, counter):
+    new_barrier = Barrier(x_velocity=-3 + speed_change)
+    new_barrier.height = random.randint(15, 45)
+    barriers.add(new_barrier)
+    return barriers
+
+
+def player_generator():
+    players = pygame.sprite.Group()
+    for i in range(100):
+        player = Player(velocity=random.randrange(1, 6),
+                        color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        players.add(player)
+    return players
+
+
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("TRex")
 barriers = pygame.sprite.Group()
-#barriers.add(barrier)
-speed_change=0
-barrier_speed=15;
-counter=0
-
-
-player = Player(win)
-player_2 = Player(win, velocity=4, color=colours["Red"])
-players = pygame.sprite.Group()
-players.add(player)
-players.add(player_2)
-
+speed_change = 0
+counter = 1
+players = player_generator()
 run = True
-
+timer = datetime.now()
 clock = pygame.time.Clock()
+
 while run:
-    if counter % 100 == 0:
-        speed_change -= 0.2
-        barrier_speed+=5
+    secondary_timer = datetime.now()
+    diff = secondary_timer - timer
+    diff = diff.total_seconds()
+    if diff >= 1.3 - 0.5 * atan(counter / 10):
+        timer = datetime.now()
+        barriers = barrier_generator(barriers, speed_change, counter)
+        counter += 1
+    if counter % 5 == 0:
+        speed_change -= 0.01
+    for s in players.sprites():
+        if pygame.sprite.spritecollideany(s, barriers) is not None:
+            players.remove(s)
+    if not players:
+        run == False
+        pygame.quit()
+        break
     clock.tick(100)
     win.fill(colours["White"])
     players.update()
-    barriers.update()
-    for s in barriers.sprites():
-        if s.rect.x<0-s.rect.width:
-            barriers.remove(s)
-    if counter % barrier_speed == 0:
-        new_barrier = Barrier()
-        new_barrier.x_velocity+=speed_change
-        print (new_barrier.height)
-        new_barrier.height = random.randint(15, 45)
-        barriers.add(new_barrier)
-    
+    barriers = outside_frame(barriers)
     barriers.draw(win)
     players.draw(win)
     pygame.display.update()
-    counter+=1
 
 pygame.quit()
